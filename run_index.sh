fc53229b8f6d263b1135b51a1dd7b817c6163ad1
besty99 <47911511+besty99@users.noreply.github.com>
2021-09-29 15:54:58 +0800

    Update run_index.sh

--- run_index.sh
+++ run_index.sh
@@ -14,3 +14,7 @@ java -jar /opt/kythe/kythe-v0.0.53/indexers/java_indexer.jar \
 
 /opt/kythe/kythe-v0.0.53/tools/write_entries  --graphstore leveldb:$KYTHE_OUTPUT_DIRECTORY_GS < $KYTHE_OUTPUT_DIRECTORY/entries
 /opt/kythe/kythe-v0.0.53/tools/write_tables --graphstore $KYTHE_OUTPUT_DIRECTORY_GS --out $KYTHE_OUTPUT_DIRECTORY_SERVING
+/opt/kythe/kythe-v0.0.53/tools/http_server \
+  --public_resources /home/miWorkSpace/kythe-test/ctest/web/ui/resources/public \
+  --listen :8080 \
+  --serving_table $KYTHE_OUTPUT_DIRECTORY_SERVING
